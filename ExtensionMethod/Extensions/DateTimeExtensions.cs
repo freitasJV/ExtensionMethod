29191e3f99992c3c4bfc3a18f91222573fde3e8f
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-19 19:30:07 -0300

    Add project files.

---
+++ ExtensionMethod/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    static class DateTimeExtensions
+    {
+        /// <summary>
+        /// A classe estática, implementando um método estático,
+        /// recebendo como parâmetro o tipo ao qual se deseja fazer o 
+        /// extension method
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if (duration.TotalHours < 24)
+                return $"{duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture)} horas";
+            else
+                return $"{duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture)} dias";
+        }
+    }
+}
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 4, 19, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
